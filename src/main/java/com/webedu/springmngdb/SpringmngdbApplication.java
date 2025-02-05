a3530f5b04d4b15e996288020403984743c4e761
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-02-05 10:59:59 -0300

    project created

---
+++ src/main/java/com/webedu/springmngdb/SpringmngdbApplication.java
@@ -0,0 +1,13 @@
+package com.webedu.springmngdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmngdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmngdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webedu/springmngdb/SpringmngdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webedu.springmngdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmngdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
