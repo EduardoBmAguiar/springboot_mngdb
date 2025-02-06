e242d385019b6282ecad64bc1b4b476eb6208d07
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-02-06 11:21:06 -0300

    User insertion with post

--- src/main/java/com/webedu/springmngdb/services/UserService.java
+++ src/main/java/com/webedu/springmngdb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.webedu.springmngdb.services;
 
 import com.webedu.springmngdb.domain.User;
+import com.webedu.springmngdb.dto.UserDTO;
 import com.webedu.springmngdb.repository.UserRepository;
 import com.webedu.springmngdb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
