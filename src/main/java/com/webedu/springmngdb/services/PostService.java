e2f9eae3e23be9aaf2a48a6877adf28724fe5122
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-02-10 13:52:08 -0300

    Getting a post by id

---
+++ src/main/java/com/webedu/springmngdb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.webedu.springmngdb.services;
+
+import com.webedu.springmngdb.domain.Post;
+import com.webedu.springmngdb.repository.PostRepository;
+import com.webedu.springmngdb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+    }
+}
